6fcc4b9520da2e46c7ddde5740f92c3d1b4d6862
Deepak Yadav <76097094+deepktp@users.noreply.github.com>
2025-10-13 13:36:37 +0530

    add direct alias to modules so we don't have to build packages each time to see changes in example app

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/thevikalp-designable/',
+  resolve: {
+    alias: {
+      '@thevikalp/designable-core': path.resolve(__dirname, '../../packages/core/src'),
+      '@thevikalp/designable-react': path.resolve(__dirname, '../../packages/react/src'),
+      '@thevikalp/designable-react-settings-form': path.resolve(__dirname, '../../packages/react-settings-form/src'),
+      '@thevikalp/designable-shared': path.resolve(__dirname, '../../packages/shared/src'),
+      '@thevikalp/designable-formily-antd': path.resolve(__dirname, '../../formily/antd/src'),
+      '@thevikalp/designable-formily-transformer': path.resolve(__dirname, '../../formily/transformer/src'),
+    },
+  },
 })
